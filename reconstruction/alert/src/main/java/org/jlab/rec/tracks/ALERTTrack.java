bd2fca9b7dcb1f18cc6001a6058b7cb8537f2079
Michael Nycz <mnycz@jlab.org>
2020-12-11 09:46:50 -0500

    Update ALERTTrack

--- reconstruction/alert/src/main/java/org/jlab/rec/tracks/ALERTTrack.java
+++ reconstruction/alert/src/main/java/org/jlab/rec/tracks/ALERTTrack.java
@@ -1,4 +1,56 @@
 package org.jlab.rec.tracks;
 
+import org.jlab.detector.hits.DetHit;
+import org.jlab.geometry.prim.Line3d;
+import org.jlab.rec.hit.ALERT.ALERTHit;
+
 public class ALERTTrack {
-}
+    private int    Id;
+    private Line3d Line;
+    private double Path;
+    private DetHit Hit;
+
+    public ALERTTrack() {
+    }
+
+    public ALERTTrack(int Id, Line3d Line, double Path) {
+        this.Id = Id;
+        this.Line = Line;
+        this.Path = Path;
+    }
+
+
+        public Line3d getLine() {
+            return Line;
+        }
+
+        public void setLine(Line3d Line) {
+            this.Line = Line;
+        }
+
+        public double getPath() {
+            return Path;
+        }
+
+        public void setPath(double Path) {
+            this.Path = Path;
+        }
+
+        public int getId() {
+            return Id;
+        }
+
+        public void setId(int Id) {
+            this.Id = Id;
+        }
+
+        public DetHit getHit(){
+            return ALERTHit;
+        }
+
+
+        public void setHit(DetHit Hit) {
+            this.Hit = Hit;
+        }
+    }
+
